5a077d8dab9599cb2cc7103201c5c8c0e93725f1
samuel <sbacon803@gmail.com>
2022-08-15 23:22:13 -0600

    add http gem, allow search from query params, secure credentials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
   def index
-    render json: { message: "hello" }
+    p Rails.application.credentials
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&from=2022-08-15&to=2022-08-15&sortBy=popularity&apiKey=b10205e9c4c947fa99a8bcf9f973da7f&language=en")
+    articles = response.parse(:json)
+    render json: articles.as_json
   end
 end
